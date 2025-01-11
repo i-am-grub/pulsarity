9a88fec62a4c1899aa1e795079c6decf5ee9c1fb
Bryce Gruber <dev@brycegruber.com>
2025-01-11 12:35:47 -0600

    cleanup test

--- tests/test_general/test_event_broker.py
+++ tests/test_general/test_event_broker.py
@@ -1,16 +1,14 @@
 import pytest
-from asyncio import Event
 
 from prophazard.extensions import RHApplication
 from prophazard.events import EventBroker, EventSetupEvt, RaceSequenceEvt
 
 
-async def broker_subscriber(event: Event, broker: EventBroker, check_values: list):
+async def broker_subscriber(broker: EventBroker, check_values: list):
 
     assert len(check_values) != 0
 
     async for message in broker.subscribe():
-        await event.wait()
 
         assert message[4] == check_values.pop(0)
 
@@ -20,18 +18,14 @@ async def broker_subscriber(event: Event, broker: EventBroker, check_values: lis
     assert len(check_values) == 0
 
 
-def broker_publisher(event: Event, broker: EventBroker, event_values: tuple[tuple]):
+def broker_publisher(broker: EventBroker, event_values: tuple[tuple]):
     for value in event_values:
         broker.publish(*value)
-
-    event.set()
 
 
 @pytest.mark.asyncio
 async def test_single_event_handling(app: RHApplication):
-    event = Event()
     broker = EventBroker()
-    event.clear()
 
     events = [EventSetupEvt.PILOT_ADD]
     values = [{"id": 1}]
@@ -39,15 +33,13 @@ async def test_single_event_handling(app: RHApplication):
     event_values = tuple(zip(events, values))
 
     async with app.test_app():
-        app.add_background_task(broker_subscriber(event, broker, values))
-        broker_publisher(event, broker, event_values)
+        app.add_background_task(broker_subscriber(broker, values))
+        broker_publisher(broker, event_values)
 
 
 @pytest.mark.asyncio
 async def test_multi_event_handling(app: RHApplication):
-    event = Event()
     broker = EventBroker()
-    event.clear()
 
     events = [EventSetupEvt.PILOT_ADD] * 3
     values = [{"id": 1}] * 3
@@ -62,5 +54,5 @@ async def test_multi_event_handling(app: RHApplication):
     test_order = [{"id": 1}, {"id": 5}, {"id": 1}, {"id": 1}]
 
     async with app.test_app():
-        app.add_background_task(broker_subscriber(event, broker, test_order))
-        broker_publisher(event, broker, event_values)
+        app.add_background_task(broker_subscriber(broker, test_order))
+        broker_publisher(broker, event_values)
