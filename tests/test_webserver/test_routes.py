97f1af9d835ed1c9bd1a63233b2a35fcef21a2c4
Bryce Gruber <dev@brycegruber.com>
2025-01-12 16:37:23 -0600

    single webserver test

--- tests/test_webserver/test_routes.py
+++ tests/test_webserver/test_routes.py
@@ -23,92 +23,92 @@ async def test_webserver_login_valid(
     return reset_required
 
 
-@pytest.mark.asyncio
-async def test_webserver_login_invalid(
-    client: TestClientProtocol, default_user_creds: tuple[str]
-):
+# @pytest.mark.asyncio
+# async def test_webserver_login_invalid(
+#     client: TestClientProtocol, default_user_creds: tuple[str]
+# ):
 
-    fake_password = "fake_password"
-    login_data = {"username": default_user_creds[0], "password": fake_password}
-    response = await client.post("/api/login", json=login_data)
-    assert response.status_code == 200
+#     fake_password = "fake_password"
+#     login_data = {"username": default_user_creds[0], "password": fake_password}
+#     response = await client.post("/api/login", json=login_data)
+#     assert response.status_code == 200
 
-    data = await response.get_json()
+#     data = await response.get_json()
 
-    assert data["status"] is False
-    assert data["password_reset_required"] is None
-
-
-@pytest.mark.asyncio
-async def test_password_reset_invalid(
-    app: RHApplication, default_user_creds: tuple[str]
-):
-    client: TestClientProtocol = app.test_client()
-
-    database = await app.get_user_database()
-    user = await database.users.get_by_username(None, default_user_creds[0])
-    assert user is not None
-
-    async with authenticated_client(client, user.auth_id.hex):
-        password = "password"
-        assert password != default_user_creds[1]
-
-        data = {"old_password": password, "new_password": "new_password"}
-
-        response = await client.post("/api/reset-password", json=data)
-        assert response.status_code == 200
-
-        data = await response.get_json()
-        assert data["status"] is False
+#     assert data["status"] is False
+#     assert data["password_reset_required"] is None
 
 
-@pytest.mark.asyncio
-async def test_password_reset_valid(app: RHApplication, default_user_creds: tuple[str]):
-    client: TestClientProtocol = app.test_client()
-    new_password = "new_password"
+# @pytest.mark.asyncio
+# async def test_password_reset_invalid(
+#     app: RHApplication, default_user_creds: tuple[str]
+# ):
+#     client: TestClientProtocol = app.test_client()
 
-    database = await app.get_user_database()
-    user = await database.users.get_by_username(None, default_user_creds[0])
-    assert user is not None
+#     database = await app.get_user_database()
+#     user = await database.users.get_by_username(None, default_user_creds[0])
+#     assert user is not None
 
-    reset_required = await test_webserver_login_valid(client, default_user_creds)
-    assert reset_required is True
+#     async with authenticated_client(client, user.auth_id.hex):
+#         password = "password"
+#         assert password != default_user_creds[1]
 
-    async with authenticated_client(client, user.auth_id.hex):
+#         data = {"old_password": password, "new_password": "new_password"}
 
-        reset_data = {
-            "old_password": default_user_creds[1],
-            "new_password": new_password,
-        }
+#         response = await client.post("/api/reset-password", json=data)
+#         assert response.status_code == 200
 
-        response = await client.post("/api/reset-password", json=reset_data)
-        assert response.status_code == 200
-
-        data = await response.get_json()
-        assert data["status"] is True
-
-    new_creds = (default_user_creds[0], new_password)
-    reset_required = await test_webserver_login_valid(client, new_creds)
-    assert reset_required is False
+#         data = await response.get_json()
+#         assert data["status"] is False
 
 
-@pytest.mark.asyncio
-async def test_pilot_stream(app: RHApplication, default_user_creds: tuple[str]):
-    client: TestClientProtocol = app.test_client()
+# @pytest.mark.asyncio
+# async def test_password_reset_valid(app: RHApplication, default_user_creds: tuple[str]):
+#     client: TestClientProtocol = app.test_client()
+#     new_password = "new_password"
 
-    race_database = await app.get_race_database()
-    await race_database.pilots.add_many(None, 3)
+#     database = await app.get_user_database()
+#     user = await database.users.get_by_username(None, default_user_creds[0])
+#     assert user is not None
 
-    user_database = await app.get_user_database()
-    user = await user_database.users.get_by_username(None, default_user_creds[0])
-    assert user is not None
+#     reset_required = await test_webserver_login_valid(client, default_user_creds)
+#     assert reset_required is True
 
-    async with authenticated_client(client, user.auth_id.hex):
-        async with client.request("/api/pilot/all") as connection:
+#     async with authenticated_client(client, user.auth_id.hex):
 
-            data = await connection.receive()
-            data_ = json.loads(data.decode())
-            assert data_ is not None
-            assert connection.status_code == 200
+#         reset_data = {
+#             "old_password": default_user_creds[1],
+#             "new_password": new_password,
+#         }
 
-            await connection.disconnect()
+#         response = await client.post("/api/reset-password", json=reset_data)
+#         assert response.status_code == 200
+
+#         data = await response.get_json()
+#         assert data["status"] is True
+
+#     new_creds = (default_user_creds[0], new_password)
+#     reset_required = await test_webserver_login_valid(client, new_creds)
+#     assert reset_required is False
+
+
+# @pytest.mark.asyncio
+# async def test_pilot_stream(app: RHApplication, default_user_creds: tuple[str]):
+#     client: TestClientProtocol = app.test_client()
+
+#     race_database = await app.get_race_database()
+#     await race_database.pilots.add_many(None, 3)
+
+#     user_database = await app.get_user_database()
+#     user = await user_database.users.get_by_username(None, default_user_creds[0])
+#     assert user is not None
+
+#     async with authenticated_client(client, user.auth_id.hex):
+#         async with client.request("/api/pilot/all") as connection:
+
+#             data = await connection.receive()
+#             data_ = json.loads(data.decode())
+#             assert data_ is not None
+#             assert connection.status_code == 200
+
+#             await connection.disconnect()
